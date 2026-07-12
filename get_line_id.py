0ebbaa818112c1095ed994f38f3133d013786dc4
ezehall <nujelm.den@gmail.com>
2026-07-13 01:21:08 +0900

    get_line_id.py

--- get_line_id.py
+++ get_line_id.py
@@ -1,1 +1,15 @@
+from flask import Flask, request
+import os
 
+app = Flask(__name__)
+
+@app.route("/callback", methods=["POST"])
+def callback():
+    body = request.json
+
+    print(body)
+
+    return "OK"
+
+if __name__ == "__main__":
+    app.run()
