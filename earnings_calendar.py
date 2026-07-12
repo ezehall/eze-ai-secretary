7210edbad9ca9833f0d65541fef3f2113b72210a
ezehall <nujelm.den@gmail.com>
2026-07-13 03:22:39 +0900

    Update earnings_calendar.py

--- earnings_calendar.py
+++ earnings_calendar.py
@@ -1,1 +1,37 @@
+import datetime
 
+
+def get_upcoming_earnings():
+
+    # 仮データ
+    # 後でAPI取得に変更
+
+    earnings = [
+        {
+            "ticker": "NVDA",
+            "name": "NVIDIA",
+            "date": "2026-08-20",
+            "importance": "★★★★★"
+        },
+        {
+            "ticker": "GOOGL",
+            "name": "Alphabet",
+            "date": "2026-07-28",
+            "importance": "★★★★"
+        }
+    ]
+
+    today = datetime.date.today()
+
+    upcoming = []
+
+    for item in earnings:
+        date = datetime.date.fromisoformat(item["date"])
+
+        diff = (date - today).days
+
+        if 0 <= diff <= 14:
+            item["days_left"] = diff
+            upcoming.append(item)
+
+    return upcoming
