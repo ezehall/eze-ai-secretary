62c2562284c26c51ad29f3772c13ef6a1041b850
ezehall <nujelm.den@gmail.com>
2026-07-13 11:40:41 +0900

    Update news_data.py

--- news_data.py
+++ news_data.py
@@ -1,6 +1,7 @@
 import urllib.parse
 import urllib.request
 import xml.etree.ElementTree as ET
+from datetime import datetime, timezone, timedelta
 
 
 def get_news(keyword, limit=3):
@@ -21,8 +22,39 @@ def get_news(keyword, limit=3):
         news = []
 
         for item in root.findall(".//item")[:limit]:
-            title = item.find("title").text
-            news.append(title)
+
+    title = item.find("title").text
+
+    pub_date = item.find("pubDate").text
+
+    try:
+        # RSS日時を解析
+        dt = datetime.strptime(
+            pub_date,
+            "%a, %d %b %Y %H:%M:%S %Z"
+        )
+
+        # UTC → 日本時間
+        japan_time = dt.replace(
+            tzinfo=timezone.utc
+        ).astimezone(
+            timezone(timedelta(hours=9))
+        )
+
+        formatted_date = japan_time.strftime(
+            "%Y年%m月%d日 %H:%M"
+        )
+
+    except:
+        formatted_date = "日時取得不可"
+
+
+    news.append(
+        {
+            "title": title,
+            "date_japan": formatted_date
+        }
+    )
 
         return news
 
